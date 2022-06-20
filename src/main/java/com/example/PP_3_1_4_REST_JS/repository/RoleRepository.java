a1c26492a6934ad75b7c488aa7fe74f7d32bfde1
Aleks-NT <mr.very@yandex.ru>
2022-06-20 19:52:23 +0500

    Done

--- src/main/java/com/example/PP_3_1_4_REST_JS/repository/RoleRepository.java
+++ src/main/java/com/example/PP_3_1_4_REST_JS/repository/RoleRepository.java
@@ -2,9 +2,9 @@ package com.example.PP_3_1_4_REST_JS.repository;
 
 import com.example.PP_3_1_4_REST_JS.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String role);
 }
